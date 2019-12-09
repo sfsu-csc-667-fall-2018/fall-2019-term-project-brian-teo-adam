1c925c58f407374f9024a5e0f7e2008f02b2f468
Brian Lopez <brianlopez@Brians-MacBook-Pro.local>
2019-12-09 15:59:41 -0800

    Created gameCardsPlayed file for game logic, but is not yet completed.

---
+++ uno_game_logic/gameCardsPlayed.js
@@ -0,0 +1,22 @@
+/*
+//gamePlayedCardsPile
+let UnoPileOfCards = require('./UnoPileOfCards'); //don't have these yet
+*/
+
+/*
+module.exports =  class gameCardsPlayed extends UnoPileOfCards { //Do not have extension file yet. 
+  constructor() {
+    super();
+  }
+
+  receiveKPlayedCards(playedCards) { //Where do all these come from?
+    this.insertCards(playedCards);
+  }
+
+  //Top of deck is the logo side facing you represented by the end of array
+  //Bottom of deck is where the deck shows the value represented by front of array
+  readBottomOfDeck() {
+    return this.deckArray[0].getCardAttributes();
+  }
+}
+*/
