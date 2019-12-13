0003b8aa186cdd2837baaa1e3d4da0c4a1290c9a
Brian Lopez <brianlopez@sfs-wifi-dhcp-10-143-196-8.wifi.sfsu.edu>
2019-12-12 16:45:05 -0800

    Created gameCardsStack file, but not completely implemented yet.

---
+++ uno_game_logic/gameCardsStack.js
@@ -0,0 +1,36 @@
+module.exports =  class gameCardsStack {
+    constructor() {
+      this.deckArray = [];
+    }
+  
+    getNumOfCardsLeft() { //Look for these functions elsewhere
+      return this.deckArray.length;
+    }
+  
+    //Cards on top of the deck display the logo, while the bottom displays the card attributes.
+    getKCardsFromDeck(n, cardsOnTop = true) {
+      if(n > this.deckArray.length) {
+        throw("Could not draw cards; the deck is empty");
+      }
+  
+      let returningCards = [];
+      if(cardsOnTop) {
+        returningCards = this.deckArray.splice(this.deckArray.length - n, n);
+      }
+      else {
+        returningCards = this.deckArray.splice(0, n);
+      }
+      return returningCards;
+    }
+  
+    insertCards(insertingCards, fromCardsOnTop = false) {
+      for (let chosenCard of insertingCards) {
+        if(fromCardsOnTop) {
+          this.deckArray.push(chosenCard);
+        }
+        else {
+          this.deckArray.unshift(chosenCard);
+        }
+      }
+    }
+  };
