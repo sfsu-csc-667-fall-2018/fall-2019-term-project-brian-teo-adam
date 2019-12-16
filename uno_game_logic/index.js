45fdbb47212d065697740547a59934e4ac13151c
Brian Lopez <brianlopez@Brians-MBP.lan>
2019-12-15 17:50:22 -0800

    index.js in uno_game_logic is up to date

--- uno_game_logic/index.js
+++ uno_game_logic/index.js
@@ -1,18 +1,32 @@
-//Edited to suit the current files had, must check back soon. 
-const gameUtilities        = require('./gameUtilities'     );
+//Index for all files in uno_game_logic folder
+
 const gamecards            = require('./gamecards'         );
-const gameaction           = require('./gameaction'        );
-const gamedeck             = require('./gamedeck'         );
+const gameCardsStack       = require('./gameCardsStack'    );
+const gameUtilities        = require('./gameUtilities'     );
+const gameAction           = require('./gameAction'        );
+const gameActionCheck      = require('./gameActionCheck'   );
+const gamedeck             = require('./gamedeck'          );
 const gameDrawCards        = require('./gameDrawCards'     );
+const gameCardsInHand      = require('./gameCardsInHand'   );
 const gameCardsPlayed      = require('./gameCardsPlayed'   );
+const gameRoom             = require('./gameRoom'          );
 const gameboard            = require('./gameboard'         );
+const gamePlayer           = require('./gamePlayer'        );
+const gameSeats            = require('./gameSeats'         );
 
 module.exports ={
-   gameUtilities: gameUtilities,
-   cardsUno: gamecards, //if not cardsUno then gamecards
-   gameaction,
-   gamedeck: gamedeck,
-   gameDrawCards: gameDrawCards,
-   gameCardsPlayed: gameCardsPlayed,
+
+   gamecards,
+   gameCardsStack,
+   gameUtilities,
+   gameAction,
+   gameActionCheck,
+   gamedeck,
+   gameDrawCards,
+   gameCardsInHand,
+   gameCardsPlayed,
+   gameRoom,
    gameboard,
+   gamePlayer,
+   gameSeats
 }
