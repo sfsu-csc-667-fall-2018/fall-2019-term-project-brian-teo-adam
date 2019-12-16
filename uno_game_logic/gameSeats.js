31d35bb7c3803a353953113234ea515ef8d8afdc
Brian Lopez <brianlopez@Brians-MBP.lan>
2019-12-15 19:06:28 -0800

    Cleaned up gameSeats.js and added comments

--- uno_game_logic/gameSeats.js
+++ uno_game_logic/gameSeats.js
@@ -1,37 +1,42 @@
-//Functions/methods need to be reviewed.
-
 module.exports = class gameSeats {
     constructor() {
       this.playersInGameArray = [];
     }
   
-    getNumOfPlayers() {
+    //Getting the players available in the array
+    getTotalPlayers() {
       return this.playersInGameArray.length;
     }
   
-    getPlayerAt(positionOfPlayer) {
+    //Getting position of player seat in array
+    getSeatOfPlayer(positionOfPlayer) {
       return this.playersInGameArray[positionOfPlayer];
     }
   
-    addPlayer(playerAdded) {
+    //Adding to the array
+    addNewPlayer(playerAdded) {
       this.playersInGameArray.push(playerAdded);
     }
-  
-    getPlayerIndex(username){ //Change to player position?
+
+    //Getting Index in Array
+    getIndexOfPlayer(username){ 
       let playerPosition = -1;
-      for(let i=0; i<this.playersInGameArray.length; i++){
-        if(this.playersInGameArray[i].name === username ){
+
+      for(let i = 0; i < this.playersInGameArray.length; i++) {
+
+        if(this.playersInGameArray[i].name === username ) {
           playerPosition = i;
           break;
+
         }
       }
-      if(playerPosition < 0){
+
+      if(playerPosition < 0) {
         throw "No player by that name can be found in a seat";
+
       }
+
       return playerPosition;
-    }
-  
-    setDealerPosition() {
-  
+
     }
   };
